56e5ea5ed6b2bd914d783affc07f05e25a6da674
Karen Darbinyan <kar31100@gmail.com>
2022-01-24 11:05:02 +0400

    added crud methods for table controller,
    mocked data need to generate using getMethod and after that everything will work

--- src/main/java/com/example/demo3/controller/TableController.java
+++ src/main/java/com/example/demo3/controller/TableController.java
@@ -8,62 +8,71 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.*;
-import java.util.stream.Stream;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class TableController extends HttpServlet {
+
+    private List<Table> lst = new LinkedList<>();
+    private static Random random = new Random();
 
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("this is GET method in general");
 
-        int ID = 0;
-        Random random = new Random();
-
-        List<Table> lst = new LinkedList<>();
-
-        for (int i = 0; i < 10; i++) {
-            lst.add(new Table(++ID, ID, random.nextInt(10), random.nextBoolean()));
-        }
-
-        List<Table> lst1 = new LinkedList<>();
-
-        for (Table item :
-                lst) {
-            if(item.isBusy()==false){
-                lst1.add(item);
+        if (lst.size() == 0) {
+            for (int i = 0; i < 10; i++) {
+                lst.add(new Table(i, i + 1, random.nextInt(10) + 1, random.nextBoolean()));
             }
+            System.out.println(lst);
+
         }
 
-        Gson gson = new Gson();
-        String tableInfo = gson.toJson(lst) + "\nfilltered data\n"+ gson.toJson(lst1);
-
-        PrintWriter writer = resp.getWriter();
-        writer.println(tableInfo);
+        resp.getWriter().println(new Gson().toJson(lst));
 
 
     }
-
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String login = req.getParameter("login");
-        String password = req.getParameter("password");
-        String id = req.getParameter("id");
-
-        Map<String, String> map = new HashMap();
-        map.put("login", login);
-        map.put("password", password);
-        map.put("id", id);
+        resp.getWriter().println("this is POST method in general");
 
 
-        Gson gson = new Gson();
-        PrintWriter writer = resp.getWriter();
-        String s = gson.toJson(map);
-        System.out.println(s);
-        writer.println(s);
+        int number = Integer.parseInt(req.getParameter("number"));
+        int seats = Integer.parseInt(req.getParameter("seats"));
+        boolean busy = Boolean.parseBoolean(req.getParameter("busy"));
+
+        int id = lst.get(lst.size() - 1).getId() + 1;
+//
+        Table data = new Table(id, number, seats, busy);
+        lst.add(data);
+        System.out.println(lst);
+
+        resp.getWriter().println(lst);
 
 
     }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("this is PUT method in general");
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("this is DELETE method in general");
+
+        int delId = Integer.parseInt(req.getParameter("idToDelete"));
+        List<Table> collect = lst.stream().filter(item -> item.getId() == delId).collect(Collectors.toList());
+
+        lst.removeAll(collect);
+
+        resp.getWriter().println(lst);
+
+    }
+
+
 }
